ebf257c107f206fc81b01c6545ed4f7527061c25
genesis-rey <genesis.bustamante@hospitalitaliano.org.ar>
2025-05-23 23:05:20 -0300

    Add files via upload

---
+++ factorial MIENTRAS-PARA.cpp
@@ -0,0 +1,40 @@
+// factorial MIENTRAS-PARA.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, factorial = 1;
+
+    cout << "Digite el numero al cual le desea sacar el factorial: ";
+    cin >> n;
+
+    // Usando estructura FOR (Para)
+    factorial = 1;
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+    cout << "1. Resultado usando FOR: " << factorial << endl;
+
+    // Usando estructura WHILE (Mientras)
+    factorial = 1;
+    int i = 1;
+    while (i <= n) {
+        factorial *= i;
+        i++;
+    }
+    cout << "2. Resultado usando WHILE: " << factorial << endl;
+
+    return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
