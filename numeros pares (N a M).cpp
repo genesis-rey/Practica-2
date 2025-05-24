9d4ed102937f82a4155cabe8d337ac189c9130f6
genesis-rey <genesis.bustamante@hospitalitaliano.org.ar>
2025-05-23 23:41:34 -0300

    Add files via upload

---
+++ numeros pares (N a M).cpp
@@ -0,0 +1,44 @@
+// numeros pares (N a M).cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M;
+
+    cout << "Ingrese el valor de N: ";
+    cin >> N;
+
+    cout << "Ingrese el valor de M: ";
+    cin >> M;
+
+    // Asegurar que N sea menor que M, si no, intercambiarlos
+    if (N >= M) {
+        cout << "Intercambiando valores ya que N no es menor que M.\n";
+        int temp = N;
+        N = M;
+        M = temp;
+    }
+
+    cout << "Numeros pares desde " << N << " hasta " << M << ":\n";
+    for (int i = N; i <= M; i++) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
+
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
